d147ee9f125e666afed2f05b6b60877ca2dfd4c4
Evan Sun <evansun06@outlook.com>
2025-08-24 15:07:25 -0600

    created some simple page layouts for the about page

--- my-website/src/pages/About/About.jsx
+++ my-website/src/pages/About/About.jsx
@@ -1,11 +1,28 @@
 import Navbar from '../../components/NavBar/Navbar.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
+import headshot from '../../assets/headshot.jpeg'
+import styles from './About.module.css'
 
 
 function About() {
     return (
         <>
-            <h2>About page WIP!</h2>
+        <div className={styles.container}>
+            <div className={styles.pol}>
+                <div className={styles.imgContainer}>
+                    <img alt="headshot" src={headshot}/>
+                </div>
+                <a href="https://github.com/evansun06" className={styles.Link}>Github</a>
+                <a href="https://www.linkedin.com/in/evan-s-5a5bb8284/" className={styles.Link}>LinkedIn</a>                
+            </div>
+            <div className={styles.about}>
+                <h3>Here's a little more about me!</h3>
+                <p> I'm currently a second year student at the University of British Columbia studying computer science. In my free time I love to draw, sail, play badminton.</p>
+
+            </div>
+    
+        </div>
+            
         </>
     );
 
